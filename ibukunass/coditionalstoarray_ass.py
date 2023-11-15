36a10ee305d86daf1e652cb54b7b8aa55c101f6d
ibukun <lolabash3@gmail.com>
2023-11-15 22:06:05 +0100

    conditionasls/datastructure

---
+++ ibukunass/coditionalstoarray_ass.py
@@ -0,0 +1,10 @@
+# Python conditionals are statements that allow you to control the flow of your program based on certain conditions. 
+# You can use the if, elif, and else keywords to create conditional statements in Python.
+# # A simple conditional statement 
+# x = 10
+# if x > 0
+# print("x is positive")
+#elif x < 0:
+# print("x is negative")
+# else:
+# print("x is zero")
--- ibukunass/datastructure_ass.py
+++ ibukunass/datastructure_ass.py
@@ -1,1 +1,23 @@
 # first assignment on data types
+# LIST
+# A list in Python is a collection of items that can be of different types, such as numbers, strings, or other lists.  
+# Lists are mutable, which means they can be changed or modified in place. Lists are created by placing all the items inside square brackets [].
+# Fruits = ["apple", "banana", "orange", "mango"]|
+# For example add grape to the fruits list 
+# fruits.append("grape")
+# print(fruits) # ["apple", "banana", "orange", "mango", "grape"]
+# TUPLE
+# A tuple in Python is a collection of items that can be of different types, such as numbers, strings, or other tuples. 
+# Tuples are immutable, which means they cannot be changed or modified in place. Tuples are created by placing all the items inside parentheses ().
+# my_tuple = (1, "Hello World", 3.5)
+# print(my_tuple)
+# SET
+# A set in Python is a collection of objects that are unordered, unchangeable, and do not allow duplicates.
+# my_set = {"apple", "banana", "cherry"}
+# my_set.add("orange")
+# print(my_set)
+# DICTIONARY
+# A dictionary in Python is a collection that allows us to store data in key-value pairs.
+# A dictionary is written with curly brackets, and each key-value pair is separated by a comma.
+# my_dict = {"name": "John", "age": 36, "country": "Nigeria"}
+# print(my_dict["name"])
