8231407217ee3fd6e94be4a35b51528a6323b661
ibukun <lolabash3@gmail.com>
2024-03-25 18:16:58 +0100

    astro_app

---
+++ ibukunass/astro_app.py
@@ -0,0 +1,14 @@
+# AstroBank App:
+# This app is used to transfer,withdraw and deposit money.
+# Transactions can take place on the app once you've sucessfully signed up on the app, which requires name,date of birth,bvn and pin.
+# In Python, while True creates an infinite loop, which is a loop that runs indefinitely.
+
+        
+while True:
+            user_input = input("Enter 'hi' to start page: ")
+            if  user_input == 'hi':
+                print("Welcome, how may i help you?")
+                break
+            else:
+                print("ERROR")
+    
